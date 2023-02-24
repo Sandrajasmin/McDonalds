a387c16baa01ac1b1847a1dba0bdd08d69e59a3f
Sandra Jasmin <sandra-jasmin@hotmail.com>
2023-02-24 18:42:19 +0100

    added content for homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/nav';
+import HeroImg from './components/hero';
+import ColumnLayout from './components/coulmlayout';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className='inner'>
+        <HeroImg />
+        <ColumnLayout />
+      </div>
     </div>
   );
 }
---
+++ src/components/hero.js
@@ -0,0 +1,9 @@
+function heroImg() {
+    return (
+        <div className="menu">
+            <img alt="" className="hero-img" src="https://s7d1.scene7.com/is/image/mcdonalds/1260x560_top-header_best-burger-1:accessible-carousel-desktop?resmode=sharp2"></img>
+        </div>
+    );
+}
+
+export default heroImg;
